99509de2344170d5679743d7f9dd057d42c8485a
Landon Reese <landon.j.reese@gmail.com>
2023-08-23 02:04:00 -0500

    Completed 27

--- 27.py
+++ 27.py
@@ -9,33 +9,38 @@ class Solution:
                 numbers_removed = numbers_removed + 1
         
         
-        return numbers_removed
+        return self.sortAndReturnCount(nums)
     
-    def sortFunction(self, nums: List[int]):
+    def sortAndReturnCount(self, nums: List[int]) -> int:
         print(f"Before Sort: {nums}")
-        temp = ""
-        for i in range(0, len(nums) - 1):
-            if nums[i] == "_":
-                temp = nums[i]
-                nums[i] = nums[i+1]
-                nums[i+1] = temp
+        num_count = 0
+        amount_to_append = 0
+        list_of_numbers = []
+        for item in nums:
+            if item != "_":
+                list_of_numbers.append(item)
+                num_count = num_count + 1
+            else:
+                amount_to_append = amount_to_append + 1
+
+        for i in range(0, amount_to_append):
+            list_of_numbers.append("_")
+
+        for i in range(0, len(list_of_numbers)):
+            nums[i] = list_of_numbers[i]
                 
         print(f"After Sort: {nums}")
-        
-    def returnCount(self, nums: List[int]) -> int:
-        num_count = 0
-        for i in range(0, len(nums)):
-            if nums[i] == "_":
+        return num_count
                 
             
             
         
 def main():
     solution = Solution()
-    test_list = [1, 1, 1, 1]
+    test_list = [2, 1, 2, 2, 4, 6, 7, 3, 2, 8, 2, 2, 2, 2, 2, 2, 7, 8, 9, 100, 2]
     test_int = 2
     # solution.removeElement(test_list, test_int)
-    print(f"Removed {solution.removeElement(test_list, test_int)}")
+    print(f"# of Elements {solution.removeElement(test_list, test_int)}")
     print(f"Final list: {test_list}")
     
 
