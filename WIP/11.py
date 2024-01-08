6e6f5f3f98176e1860d788c123849425733b704c
Landon Reese <landon.j.reese@gmail.com>
2024-01-08 14:30:21 -0600

    11 WIP

---
+++ WIP/11.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        volume = 0
+        
+        for i in range(0, len(height)):
+            for j in range(len(height) - 1, i, -1):
+                
+                b = j - i
+                if height[i] < height[j]:
+                    h = height[i]
+                else:
+                    h = height[j]
+                
+                if b * h > volume:
+                    volume = b * h
+                    
+        return volume
+        
+def main():
+    inputs = [
+        [1,8,6,2,5,4,8,3,7],
+        [1,1],
+        [0,0,0,0,0,0,0,0,0]
+    ]
+    
+    outputs = [
+        49,
+        1,
+        0,
+    ]
+    
+    s = Solution()
+    
+    for i in range(0, len(inputs)):
+        assert(s.maxArea(inputs[i]) == outputs[i])
+    
+if __name__ == "__main__":
+    main()
